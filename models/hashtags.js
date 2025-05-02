ace942939ba2ab26a8588b983a90d128eef8a2e1
Etienne de V <edevalmont@gmail.com>
2025-05-02 13:55:41 +0200

    models and db

---
+++ models/hashtags.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const hashtagsSchema = mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+const hashtagsModel = mongoose.model("hashtags", hashtagsSchema);
+
+module.exports = hashtagsModel;
---
+++ models/tweets.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const tweetsSchema = mongoose.Schema({
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  content: { type: String, required: true },
+  submittedAt: Date,
+  likes: { type: Number, default: 0 },
+  userLikes: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  hashtags: { type: mongoose.Schema.Types.ObjectId, ref: "hashtags" },
+});
+
+const tweetsModel = mongoose.model("tweets", tweetsSchema);
+
+module.exports = tweetsModel;
